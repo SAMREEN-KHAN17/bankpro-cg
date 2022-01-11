8605a16ecaf820f16d069c7f48e6f680ae9567e2
SAMREEN KHAN <khansamreen1726@gmail.com>
2022-01-11 18:10:48 +0530

    dashboard connection added

--- src/main/java/com/springboot/bankapp/SecurityConfig.java
+++ src/main/java/com/springboot/bankapp/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.springboot.bankapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		 PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); 
 		 return passwordEncoder;
 	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(getDBAuthenticator());
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		 .antMatchers("/customer").permitAll()
-		 
+		 .antMatchers("/user").authenticated()
 		 .anyRequest()
 		 .permitAll()
 		 .and()
